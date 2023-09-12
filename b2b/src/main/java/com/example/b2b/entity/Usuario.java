7e84d6743ab503189fd47c06d5bd1dd314d18e4a
OliveiraMG <matheus.moura@sptech.school>
2023-09-12 09:02:48 -0300

    feat: adicionando nova classe de usuario

---
+++ b2b/src/main/java/com/example/b2b/entity/Usuario.java
@@ -0,0 +1,20 @@
+package com.example.b2b.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "usuario")
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+public abstract class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String cnpj;
+    private String senha;
+}
