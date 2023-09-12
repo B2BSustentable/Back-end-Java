79da11a569345ac7e82b7ea303927f5557ac1f3a
OliveiraMG <matheus.moura@sptech.school>
2023-09-12 09:03:46 -0300

    feat: criando o repository do usuario

---
+++ b2b/src/main/java/com/example/b2b/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.b2b.repository;
+
+import com.example.b2b.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+        Usuario findByCnpj(String cnpj);
+}
