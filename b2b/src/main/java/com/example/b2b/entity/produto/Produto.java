8069516aa8bb96d17fab54d4a31f706826ad1a10
OliveiraMG <matheus.moura@sptech.school>
2023-09-14 08:07:42 -0300

    feat: Adicionando NOTBLANK

--- b2b/src/main/java/com/example/b2b/entity/produto/Produto.java
+++ b2b/src/main/java/com/example/b2b/entity/produto/Produto.java
@@ -2,6 +2,7 @@ package com.example.b2b.entity.produto;
 
 import com.example.b2b.dtos.produto.ProdutoRequestDTO;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Table(name = "produto")
@@ -13,11 +14,10 @@ import lombok.*;
 @EqualsAndHashCode(of = "id")
 public class Produto {
     @Id
+    @NotBlank
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
     private String nome;
-
     private Double preco;
 
     public Produto(ProdutoRequestDTO data){
