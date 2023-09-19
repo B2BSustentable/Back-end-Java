040daec58295d26eb16626a2d9efb6eb74a40787
OliveiraMG <matheus.moura@sptech.school>
2023-09-18 21:39:56 -0300

    refactor: Removendo construtor não canônico

--- b2b/src/main/java/com/example/b2b/dtos/usuario/RegisterResponseDTO.java
+++ b2b/src/main/java/com/example/b2b/dtos/usuario/RegisterResponseDTO.java
@@ -1,9 +1,4 @@
 package com.example.b2b.dtos.usuario;
 
 public record RegisterResponseDTO(String nome, String cnpj, String senha, String email, String tipoUsuario) {
-
-    public RegisterResponseDTO(RegisterDTO registerDTO){
-        this(registerDTO.nome(), registerDTO.cnpj(), registerDTO.senha(), registerDTO.email(), registerDTO.tipoUsuario().toString());
-    }
-
 }
