70381bc41f9c3ee566b903dcaf56154bdf9d9e9e
OliveiraMG <matheus.moura@sptech.school>
2023-11-24 15:44:10 -0300

    Refactor: Refatorando DTOs que eram para ter subido no commit de empresas e refatoração

--- b2b/src/main/java/com/example/b2b/dtos/empresa/RegisterRequestDTO.java
+++ b2b/src/main/java/com/example/b2b/dtos/empresa/RegisterRequestDTO.java
@@ -1,12 +1,14 @@
 package com.example.b2b.dtos.empresa;
 
 import com.example.b2b.entity.empresa.TipoPlanos;
+import com.example.b2b.entity.endereco.Endereco;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CNPJ;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record RegisterRequestDTO(@NotBlank String nomeEmpresa, @CNPJ String cnpj, @NotBlank @Email String email, @NotBlank String senha, String descricao, LocalDateTime dataDeCriacao, String photo, @NotNull TipoPlanos tipoPlanos, int limiteDeProdutos, double desconto, boolean suporte24h, String acessoVIP) {
+public record RegisterRequestDTO(@NotBlank String nomeEmpresa,@NotBlank @CNPJ String cnpj, @NotBlank @Email String email, @NotBlank String senha, String descricao, LocalDateTime dataDeCriacao, String photo, @NotNull TipoPlanos tipoPlanos, int qtdNegociantes, int limiteDeProdutos, boolean consultasIlimitadas, boolean addFavoritos, List<Endereco> enderecos) {
     }
