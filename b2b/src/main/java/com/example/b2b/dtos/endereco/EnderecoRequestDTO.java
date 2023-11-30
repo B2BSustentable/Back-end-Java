1863c53626b37d2047d987e558ed9394bab385c1
OliveiraMG <matheus.moura@sptech.school>
2023-11-30 10:35:13 -0300

    Refactor: Deixando passar campos nulos

--- b2b/src/main/java/com/example/b2b/dtos/endereco/EnderecoRequestDTO.java
+++ b2b/src/main/java/com/example/b2b/dtos/endereco/EnderecoRequestDTO.java
@@ -4,12 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record EnderecoRequestDTO(@NotBlank String rua,
-                                 @NotNull @Positive Integer numero,
-                                 @NotBlank String bairro,
-                                 @NotBlank String cidade,
-                                 @NotBlank String estado,
-                                 @NotBlank String pais,
+public record EnderecoRequestDTO(String rua,
+                                 @Positive Integer numero,
+                                 String bairro,
+                                 String cidade,
+                                 String estado,
+                                 String pais,
                                  @NotBlank String cep
 ) {
 }
